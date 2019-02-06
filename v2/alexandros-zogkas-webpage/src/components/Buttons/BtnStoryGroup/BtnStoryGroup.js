237bc5464074180db37c82ad8e7774252f752061
alexzg <azlex0@gmail.com>
2019-02-06 18:18:06 +0100

    Update BtnStoryGroup.js

--- v2/alexandros-zogkas-webpage/src/components/Buttons/BtnStoryGroup/BtnStoryGroup.js
+++ v2/alexandros-zogkas-webpage/src/components/Buttons/BtnStoryGroup/BtnStoryGroup.js
@@ -3,10 +3,13 @@ import React from 'react';
 import './BtnStoryGroup.css';
 
 const BtnStoryGroup = (props) => {
+	let classname = 'GroupBtnLine';
+	if (props.ToggleShow) { classname += ' GroupBtnClicked' };
+	
 	return (
-	<div className='Line' style={{borderColor: props.Color}}>
+	<div className={classname} style={{borderColor: props.Color}}>
 		<button 
-			className='Btn'
+			className='GroupBtn'
 			style={{backgroundColor: props.Color, borderColor: props.Color}}
 			onClick={() => props.OnClick(props.Type, null)}>
 			{props.Type}
