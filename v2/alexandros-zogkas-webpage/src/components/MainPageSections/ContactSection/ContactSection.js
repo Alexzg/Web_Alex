41957e4849c2193a02886a2bdf2a256839a5d458
alexzg <azlex0@gmail.com>
2019-02-06 18:18:23 +0100

    Update ContactSection.js

--- v2/alexandros-zogkas-webpage/src/components/MainPageSections/ContactSection/ContactSection.js
+++ v2/alexandros-zogkas-webpage/src/components/MainPageSections/ContactSection/ContactSection.js
@@ -2,25 +2,42 @@ import React, { Component } from 'react';
 
 import SocialMediaData from '../../../assets/ContactData/SocialMediaData/SocialMediaData';
 
-import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import './ContactSection.css';
 
 import SocialMediaBtn from '../../Buttons/SocialMediaBtn/SocialMediaBtn';
 
+import LinkedinIcon from '../../../assets/Images/ContactSection/Socialicons/linkedin.svg';
+import XingIcon from '../../../assets/Images/ContactSection/Socialicons/xing.svg';
+import GithubIcon from '../../../assets/Images/ContactSection/Socialicons/github.svg';
 
 class ContactSection extends Component {
 	render(){
+		SocialMediaData.socialButtons.map(socialBtn => {
+			let socialButton;
+			if (socialBtn.btnType === 'Xing') {
+				socialButton = XingIcon;
+			} else if (socialBtn.btnType === 'LinkedIn'){
+				socialButton = LinkedinIcon;
+			} else {
+				socialButton = GithubIcon;
+			}
+			socialBtn.icon = socialButton;
+		})
+		
 		const SocialMediaButtons = SocialMediaData.socialButtons.map(socialBtn => (
-					<SocialMediaBtn 
-						key={socialBtn.btnType}
-						Link={socialBtn.link}>
-						{socialBtn.btnType}
-					</SocialMediaBtn>));
+			<SocialMediaBtn 
+				key={socialBtn.btnType}
+				Btn={socialBtn.btnType}
+				Link={socialBtn.link}
+				Img={socialBtn.icon}>
+				{socialBtn.btnType}
+			</SocialMediaBtn>));
 		
 		return(
-			<div id={this.props.id} >
-				<h3>Contact Section</h3>
-				<p>contact info</p>
-				{SocialMediaButtons}
+			<div id={this.props.id} className='ContactSectionBody'>
+				<h3>If you want to know more...</h3>
+				<div className='SocialMediaButtonsGroup'>{SocialMediaButtons}</div>
+				<h4>...or if you want help with a project</h4>
 			</div>)
 	}
 }
