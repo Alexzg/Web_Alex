74d19d2a038e138f7d03eac8bbfc943d00e06386
alexzg <azlex0@gmail.com>
2019-02-06 18:18:41 +0100

    Update BtnStoryTitle.js

--- v2/alexandros-zogkas-webpage/src/components/Story/BtnStoryTitle/BtnStoryTitle.js
+++ v2/alexandros-zogkas-webpage/src/components/Story/BtnStoryTitle/BtnStoryTitle.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
+import './BtnStoryTitle.css';
+
 const BtnStoryTitle = (props) => {
 	let btnColor = 'black';
 	//Control of btn type and color accordingly
 	const storyGroup = props.StoryGroups.find(group => group.btnType === props.StoryGroup);
 	btnColor = storyGroup.btnColor;
 	
+	// StoryGroups <- StorylineGroups.json
+	let groupShow = storyGroup.btnToggleShow;
+	if (storyGroup.btnType === 'All'){
+		if (storyGroup.btnToggleShow === true){
+			groupShow = true;
+		}
+	};
+	
+	// If group btn is clicked, the content must be visible
+	// Else 'storyContent' visibility works as before
+	let show;
+	groupShow ? show=groupShow : show=props.ShowContent;
+	
+	let classname = 'BtnStoryTitle';
+	if (show){ classname+=' TitleClicked' };
+	
 	return(
 		<button 
-			style={{display: 'block', color: btnColor}}
+			className={classname}
+			style={{borderColor: btnColor}}
 			onClick={() => props.OnClick(null, props.StoryTitle)}>
 			{props.StoryTitle}
 		</button>
