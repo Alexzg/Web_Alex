9c8bfaf2d9aa930a2ed14243fa71079dcad404dd
alexzg <azlex0@gmail.com>
2019-03-02 12:18:46 +0100

    Update WelcomeSection.js

--- v2/alexandros-zogkas-webpage/src/components/MainPageSections/WelcomeSection/WelcomeSection.js
+++ v2/alexandros-zogkas-webpage/src/components/MainPageSections/WelcomeSection/WelcomeSection.js
@@ -3,11 +3,11 @@ import React from 'react';
 import './WelcomeSection.css';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
-import image from '../../../assets/Images/WelcomeSection/Profile.jpg';
+import image from '../../../assets/Images/WelcomeSection/Welcome.jpg';
 
 const WelcomeSection = (props) => (
 	<div id={props.id} className='MainSection'>
-		<img src={image} />
+		<img src={image} alt='A bulb on a blackboard having as drawings, circles for ideas'/>
 		<p>Hi, I am Alexandros and this is my Story so far...</p>
 	</div>
 );
